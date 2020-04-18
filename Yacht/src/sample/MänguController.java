1359a40d1fbedbbbc83eb2b5af18e699d254e492
u53rn4m3123 <krishaster9@gmail.com>
2020-04-18 11:25:27 +0300

    lisasin täringud

--- Yacht/src/sample/MänguController.java
+++ Yacht/src/sample/MänguController.java
@@ -2,13 +2,24 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class MänguController {
     @FXML
     Label käiguLabel;
 
+    @FXML
+    HBox täringuteKast;
+    @FXML
+    ImageView täring1, täring2, täring3, täring4, täring5;
+
     public void määraKäik(Mängija mängija) {
         käiguLabel.setText(mängija.getNimi());
+    }
+
+    public void määraTäringud(int[] numbrid) {
+        
     }
 
     public void veereta() {
